2eb983e448ce2a891c86a248df70d0ac9b146a9b
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-10 16:26:04 -0300

    feat: cria componente Lista de pratos

---
+++ src/components/ListaPratos/index.tsx
@@ -0,0 +1,17 @@
+import Prato from '../Prato'
+import { Container } from './style'
+
+const ListaPratos = () => (
+  <Container>
+    <div className="container">
+      <Prato />
+      <Prato />
+      <Prato />
+      <Prato />
+      <Prato />
+      <Prato />
+    </div>
+  </Container>
+)
+
+export default ListaPratos
---
+++ src/components/ListaPratos/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 40px 0px 120px;
+
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 32px;
+  }
+`
