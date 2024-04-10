17eea92d75a7933f554a9571ffe7a52155f94e04
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-10 15:24:23 -0300

    feat: cria nova rota

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Perfil from './pages/Perfil'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/pratos" element={<Home />} />
+    <Route path="/perfil" element={<Perfil />} />
   </Routes>
 )
 
