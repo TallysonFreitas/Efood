9c4e3dc526d3dea97dd7e9ee888bbbc261e8b89f
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-10 10:20:20 -0300

    style: cria estilo global e constante cores

---
+++ src/styles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  list-style:none;
+  font-family:Roboto, sans-serif;
+}
+
+body{
+  background-color:;
+}`
+
+export const cores = {
+  bege: '#fff8f2',
+  laranja: '#E66767',
+  marromClaro: '#FFEBD9',
+  branco: '#fff'
+}
