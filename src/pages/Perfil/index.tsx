2e3b5a8187799999222eddec301345a994f10afa
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-10 18:23:02 -0300

    feat: cria lista de pratos

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -2,6 +2,47 @@ import Banner from '../../components/Banner'
 import Header from '../../components/Header'
 import BannerImg from '../../assets/images/dolce.png'
 import ListaPratos from '../../components/ListaPratos'
+import { PratoType } from '../../models/Prato'
+import Margerita from '../../assets/images/pizza-marguerita.png'
+
+const Pratos: PratoType[] = [
+  {
+    descricao:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    titulo: 'Pizza Marguerita',
+    imagem: Margerita
+  },
+  {
+    descricao:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    titulo: 'Pizza Marguerita',
+    imagem: Margerita
+  },
+  {
+    descricao:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    titulo: 'Pizza Marguerita',
+    imagem: Margerita
+  },
+  {
+    descricao:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    titulo: 'Pizza Marguerita',
+    imagem: Margerita
+  },
+  {
+    descricao:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    titulo: 'Pizza Marguerita',
+    imagem: Margerita
+  },
+  {
+    descricao:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    titulo: 'Pizza Marguerita',
+    imagem: Margerita
+  }
+]
 
 const Perfil = () => (
   <>
@@ -11,7 +52,7 @@ const Perfil = () => (
       tag="Italiana"
       titulo="La Dolce Vita Trattoria"
     />
-    <ListaPratos />
+    <ListaPratos pratos={Pratos} />
   </>
 )
 
