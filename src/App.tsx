bf21ab844be1593097bf85cf661c98281b90e00e
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-10 14:23:06 -0300

    feat: implementa Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import Rotas from './routes'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }
