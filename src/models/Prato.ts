9d79401e41245c98bd762059025dbd147456778a
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-10 18:22:48 -0300

    feat: cria tipo de prato

---
+++ src/models/Prato.ts
@@ -0,0 +1,11 @@
+export class PratoType {
+  titulo: string
+  descricao: string
+  imagem: string
+
+  constructor(titulo: string, descricao: string, imagem: string) {
+    this.titulo = titulo
+    this.descricao = descricao
+    this.imagem = imagem
+  }
+}
