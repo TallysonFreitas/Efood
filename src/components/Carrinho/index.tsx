d9a9fff32ddf083bfb95933cec7a9377f5bcc25b
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-11 14:21:27 -0300

    feat: cria componentizacao do carrinho

--- src/components/Carrinho/index.tsx
+++ src/components/Carrinho/index.tsx
@@ -1,28 +1,136 @@
 import { useSelector } from 'react-redux'
-import { Card, Container } from './style'
+import {
+  Button,
+  Card,
+  Close,
+  Container,
+  Entrega,
+  Info,
+  Texto,
+  Titulo,
+  TotalPedidos
+} from './style'
 import { RootReducer } from '../../store'
 import CarrinhoItem from '../CarrinhoItem'
+import CloseImg from '../../assets/images/close.png'
+import { useState } from 'react'
+import Form from '../Form'
 
-const Carrinho = ({ visivel }: { visivel: boolean }) => {
+const Carrinho = ({
+  visivel,
+  fechar
+}: {
+  visivel: boolean
+  fechar: () => void
+}) => {
   const { pedidos } = useSelector((state: RootReducer) => {
     return state.pedidos
   })
 
+  const ValorTotal = pedidos.reduce(
+    (acumulador, each) => acumulador + each.valor,
+    0
+  )
+
+  const [etapa, setEtapa] = useState('carrinho')
+
+  function carrinho() {
+    setEtapa('carrinho')
+  }
+
+  function pagamento() {
+    setEtapa('pagamento')
+  }
+
+  function entrega() {
+    setEtapa('entrega')
+  }
+
+  function fim() {
+    setEtapa('fim')
+  }
+
   return (
     <Container visivel={visivel}>
       <Card>
-        {pedidos.map((each) => {
-          return (
-            <CarrinhoItem
-              descricao={each.descricao}
-              imagem={each.imagem}
-              preco={each.valor}
-              titulo={each.titulo}
-              key={each.id}
-              id={each.id}
-            />
-          )
-        })}
+        <Close onClick={fechar}>
+          <img src={CloseImg} alt="close" />
+        </Close>
+        {/* Carrinho */}
+        {etapa === 'carrinho' && (
+          <>
+            {pedidos.map((each) => {
+              return (
+                <CarrinhoItem
+                  descricao={each.descricao}
+                  imagem={each.imagem}
+                  preco={each.valor}
+                  titulo={each.titulo}
+                  key={each.id}
+                  id={each.id}
+                />
+              )
+            })}
+            <TotalPedidos>
+              <Texto>Valor Total</Texto>
+              <Texto>
+                {ValorTotal.toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL'
+                })}
+              </Texto>
+            </TotalPedidos>
+            <Entrega
+              onClick={() => {
+                setEtapa('entrega')
+              }}
+            >
+              Continuar com a entrega
+            </Entrega>
+          </>
+        )}
+        {/* Entrega */}
+        {etapa === 'entrega' && (
+          <>
+            <Form type="entrega" voltar={carrinho} continuar={pagamento} />
+          </>
+        )}
+        {/* Pagamento */}
+        {etapa === 'pagamento' && (
+          <>
+            <Form type="pagamento" voltar={entrega} continuar={fim} />
+          </>
+        )}
+        {/* Fim */}
+        {etapa === 'fim' && (
+          <>
+            <Titulo>Pedido realizado - ORDER_ID</Titulo>
+            <Info>
+              Estamos felizes em informar que seu pedido já está em processo de
+              preparação e, em breve, será entregue no endereço fornecido.
+            </Info>
+            <Info>
+              Gostaríamos de ressaltar que nossos entregadores não estão
+              autorizados a realizar cobranças extras.
+            </Info>
+            <Info>
+              Lembre-se da importância de higienizar as mãos após o recebimento
+              do pedido, garantindo assim sua segurança e bem-estar durante a
+              refeição.
+            </Info>
+            <Info>
+              Esperamos que desfrute de uma deliciosa e agradável experiência
+              gastronômica. Bom apetite!
+            </Info>
+            <Button
+              onClick={() => {
+                fechar()
+              }}
+            >
+              Concluir
+            </Button>
+          </>
+        )}
       </Card>
     </Container>
   )
--- src/components/Carrinho/style.ts
+++ src/components/Carrinho/style.ts
@@ -25,3 +25,77 @@ export const Card = styled.div`
   padding: 32px 8px;
   background-color: ${cores.laranja};
 `
+
+export const Close = styled.div`
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const TotalPedidos = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 40px;
+`
+
+export const Texto = styled.p`
+  color: ${cores.marromClaro};
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 16px;
+  height: 16px;
+`
+
+export const Entrega = styled.button`
+  width: 100%;
+  background-color: ${cores.marromClaro};
+  font-size: 14px;
+  border: none;
+  font-weight: 700;
+  line-height: 16px;
+  text-align: center;
+  color: ${cores.laranja};
+  padding: 4px 0;
+  margin-top: 16px;
+  cursor: pointer;
+`
+
+export const Titulo = styled.h3`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 18px;
+  text-align: left;
+  color: ${cores.marromClaro};
+`
+
+export const Info = styled.p`
+  color: ${cores.marromClaro};
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  text-align: left;
+  margin-bottom: 24px;
+`
+
+export const Button = styled.button`
+  width: 100%;
+  background-color: ${cores.marromClaro};
+  font-size: 14px;
+  border: none;
+  font-weight: 700;
+  line-height: 16px;
+  text-align: center;
+  color: ${cores.laranja};
+  padding: 4px 0;
+  margin-top: 16px;
+  cursor: pointer;
+`
