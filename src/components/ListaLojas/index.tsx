14521e5dc9b6dfcbe8079c95f054bca94db1a25b
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-15 11:56:36 -0300

    fix: corrige importacoes vindas do ajax

--- src/components/ListaLojas/index.tsx
+++ src/components/ListaLojas/index.tsx
@@ -1,23 +1,32 @@
 import * as S from './style'
-import { Loja } from '../../models/Loja'
 import LojaItem from '../Loja'
+import { RestauranteType } from '../../pages/Home'
 
-const ListaLoja = ({ lojas }: { lojas: Loja[] }) => (
-  <S.Container>
-    <div className="container">
-      {lojas.map((each) => (
-        <LojaItem
-          descricao={each.descricao}
-          imagem={each.imagem}
-          link={each.link}
-          nome={each.nome}
-          nota={each.nota}
-          tags={each.tags}
-          key={each.nome}
-        />
-      ))}
-    </div>
-  </S.Container>
-)
+const ListaLoja = ({ lojas }: { lojas: RestauranteType[] }) => {
+  function criaTags(destaque: boolean, tipo: string) {
+    if (destaque === true) {
+      return ['Destaque da semana', tipo]
+    }
+    return [tipo]
+  }
+
+  return (
+    <S.Container>
+      <div className="container">
+        {lojas.map((each) => (
+          <LojaItem
+            descricao={each.descricao}
+            imagem={each.capa}
+            id={each.id}
+            nome={each.titulo}
+            nota={each.avaliacao}
+            tags={criaTags(each.destacado, each.tipo)}
+            key={each.id}
+          />
+        ))}
+      </div>
+    </S.Container>
+  )
+}
 
 export default ListaLoja
--- src/components/ListaPratos/index.tsx
+++ src/components/ListaPratos/index.tsx
@@ -1,25 +1,29 @@
-import { PratoType } from '../../models/Prato'
+import { CardapioType } from '../../pages/Home'
 import Prato from '../Prato'
 import * as S from './style'
 
 type Props = {
-  pratos: PratoType[]
+  pratos: CardapioType[]
 }
-const ListaPratos = ({ pratos }: Props) => (
-  <S.Container>
-    <div className="container">
-      {pratos.map((each) => (
-        <Prato
-          descricao={each.descricao}
-          imagem={each.imagem}
-          titulo={each.titulo}
-          key={each.id}
-          valor={each.valor}
-          id={each.id}
-        />
-      ))}
-    </div>
-  </S.Container>
-)
+
+const ListaPratos = ({ pratos }: Props) => {
+  return (
+    <S.Container>
+      <div className="container">
+        {pratos.map((each) => (
+          <Prato
+            porcao={each.porcao}
+            descricao={each.descricao}
+            imagem={each.foto}
+            titulo={each.nome}
+            key={each.id}
+            valor={each.preco}
+            id={each.id}
+          />
+        ))}
+      </div>
+    </S.Container>
+  )
+}
 
 export default ListaPratos
