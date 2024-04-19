27389ed421b8c2d8bbde7b35c73220afc8727eaa
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-19 07:07:02 -0300

    fix: retira Modal de componente Prato

--- src/components/Prato/index.tsx
+++ src/components/Prato/index.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
 import * as S from './style'
-import Modal from '../Modal'
 
 type Props = {
   titulo: string
   imagem: string
   descricao: string
-  valor: number
-  id: number
-  porcao: string
+  abrir: () => void
 }
 
-const Prato = ({ titulo, imagem, descricao, valor, id, porcao }: Props) => {
-  function ativaModal() {
-    setVisivel(!visivel)
-  }
-
-  const [visivel, setVisivel] = useState(false)
+const Prato = ({ titulo, imagem, descricao, abrir }: Props) => {
   return (
     <>
       <S.Card>
@@ -25,18 +16,8 @@ const Prato = ({ titulo, imagem, descricao, valor, id, porcao }: Props) => {
           <S.Titulo>{titulo}</S.Titulo>
           <S.Descricao>{descricao}</S.Descricao>
         </div>
-        <S.Button onClick={ativaModal}>Mais detalhes</S.Button>
+        <S.Button onClick={abrir}>Mais detalhes</S.Button>
       </S.Card>
-      <Modal
-        porcao={porcao}
-        visivel={visivel}
-        fechar={ativaModal}
-        imagem={imagem}
-        titulo={titulo}
-        descricao={descricao}
-        valor={valor}
-        id={id}
-      />
     </>
   )
 }
