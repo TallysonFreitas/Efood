4ede4c8da791b62fd5b7c9d2e907031e03611aa4
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-19 07:08:11 -0300

    Feat: cria Modal com um unico elemento modal

--- src/components/ListaPratos/index.tsx
+++ src/components/ListaPratos/index.tsx
@@ -1,27 +1,61 @@
+import { useState } from 'react'
 import { CardapioType } from '../../pages/Home'
 import Prato from '../Prato'
 import * as S from './style'
+import Modal from '../Modal'
 
 type Props = {
   pratos: CardapioType[]
 }
 
 const ListaPratos = ({ pratos }: Props) => {
+  const modalInitialState = {
+    foto: '',
+    preco: 0,
+    id: 0,
+    nome: '',
+    descricao: '',
+    porcao: ''
+  }
+
+  const [modalProps, setModalProps] = useState<CardapioType>(modalInitialState)
+  const [modalVisivel, setModalVisivel] = useState<boolean>(false)
+
   return (
     <S.Container>
       <div className="container">
         {pratos.map((each) => (
           <Prato
-            porcao={each.porcao}
             descricao={each.descricao}
             imagem={each.foto}
             titulo={each.nome}
             key={each.id}
-            valor={each.preco}
-            id={each.id}
+            abrir={() => {
+              setModalVisivel(true),
+                setModalProps({
+                  foto: each.foto,
+                  preco: each.preco,
+                  id: each.id,
+                  nome: each.nome,
+                  descricao: each.descricao,
+                  porcao: each.porcao
+                })
+            }}
           />
         ))}
       </div>
+      <Modal
+        descricao={modalProps.descricao}
+        id={modalProps.id}
+        imagem={modalProps.foto}
+        porcao={modalProps.porcao}
+        titulo={modalProps.nome}
+        valor={modalProps.preco}
+        visivel={modalVisivel}
+        fechar={() => {
+          setModalVisivel(false), setModalProps(modalInitialState)
+        }}
+      />
     </S.Container>
   )
 }
