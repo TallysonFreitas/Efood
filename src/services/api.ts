0494a1d7ad52920e246f370dc1b471aa350e5fd5
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-21 19:19:08 -0300

    feat: cria mutation para compra

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,40 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { RestauranteType } from '../pages/Home'
 
+type Product = {
+  id: number
+  price: number
+}
+
+export type PurchaseType = {
+  products: Product[]
+  delivery: {
+    receiver: string
+    address: {
+      description: string
+      city: string
+      zipCode: string
+      number: number
+      complement: string
+    }
+  }
+  payment: {
+    card: {
+      name: string
+      number: string
+      code: number
+      expires: {
+        month: number
+        year: number
+      }
+    }
+  }
+}
+
+type PurchaseReponse = {
+  orderId: string
+}
+
 const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://fake-api-tau.vercel.app/api/efood/'
@@ -11,10 +45,21 @@ const api = createApi({
     }),
     getPratos: builder.query<RestauranteType, string>({
       query: (id) => `restaurantes/${id}`
+    }),
+    purchase: builder.mutation<PurchaseReponse, PurchaseType>({
+      query: (body) => ({
+        url: 'checkout',
+        method: 'POST',
+        body
+      })
     })
   })
 })
 
 export default api
 
-export const { useGetPratosQuery, useGetRestaurantesQuery } = api
+export const {
+  useGetPratosQuery,
+  useGetRestaurantesQuery,
+  usePurchaseMutation
+} = api
